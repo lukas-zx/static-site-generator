814bde4cece04e5efff57fa4140f2262e1f3fc75
lkzx <lukas.koenn98@googlemail.com>
2026-02-08 13:13:48 +0100

    solve lesson 2-1

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    textnode = TextNode(
+        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
+    )
+    print(textnode)
+
+
+main()
