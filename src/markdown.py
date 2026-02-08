bf8ad7e50f6694f0680aa0f893664ee907e18c01
lkzx <lukas.koenn98@googlemail.com>
2026-02-08 15:27:43 +0100

    solve lesson 3-1

---
+++ src/markdown.py
@@ -0,0 +1,23 @@
+from textnode import TextNode, TextType, text_node_to_html_node
+
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    res: list[TextNode] = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            res.append(old_node)
+
+        parts = old_node.text.split(delimiter)
+        if len(parts) < 3:
+            raise Exception(f"invalid markdown syntax: {old_node.text}")
+
+        node1 = TextNode(parts[0], TextType.TEXT)
+        node2 = TextNode(parts[1], text_type)
+        node3 = TextNode(parts[2], TextType.TEXT)
+
+        res.append(node1)
+        res.append(node2)
+        res.append(node3)
+    return res
+
+
---
+++ src/test_markdown.py
@@ -0,0 +1,18 @@
+import unittest
+
+from markdown import split_nodes_delimiter
+from textnode import TextType, TextNode
+
+class TestMarkdown(unittest.TestCase):
+    def test_code_block(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+
+if __name__ == "__main__":
+    unittest.main()
